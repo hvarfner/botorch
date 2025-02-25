f59b919c560c321b0266e2b0556945d4b0850af9
hvarfner <carl.hvarfner@live.se>
2025-02-25 14:03:26 +0100

    Bugfix of qNIPV

--- botorch/acquisition/active_learning.py
+++ botorch/acquisition/active_learning.py
@@ -115,8 +115,7 @@ class qNegIntegratedPosteriorVariance(AcquisitionFunction):
             )
 
         neg_variance = posterior.variance.mul(-1.0)
-
-        if self.posterior_transform is None:
+        if self.model.num_outputs == 1:
             # if single-output, shape is 1 x batch_shape x num_grid_points x 1
             return neg_variance.mean(dim=-2).squeeze(-1).squeeze(0)
         else:
